cdd953fb7e58b01df69695d3677ce0339d07d39d
Jamie Alexandre <jamalex@gmail.com>
2014-04-19 15:59:45 -0700

    Clear mounted filesystems.

--- create-iso.sh
+++ create-iso.sh
@@ -1,4 +1,6 @@
 sudo apt-get install uck
 sudo perl -pi -e 's/cp -f \/etc\/resolv.conf/cp -d \/etc\/resolv.conf/g' /usr/lib/uck/remaster-live-cd.sh
 
+sudo umount ~/tmp/remaster-root/home/*/.gvfs
+
 uck-gui
