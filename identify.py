860f783bd313880f36daa41af93bacd81f439fbb
Kapya <kapsakala@gmail.com>
2016-10-25 15:04:30 +0200

    Update identify.py

--- identify.py
+++ identify.py
@@ -3,7 +3,7 @@ import sqlite3
 #import version
 
 if __name__ == '__main__':
-    db = sqlite3.connect('/home/edulution/.kalite/database/data.sqlite')
+    db = sqlite3.connect('/home/edulution/.kalite/database/data.sqlite')# could change depending on name of user
     c = db.cursor()
     results = c.execute(""" 
 select d.name,d.version from securesync_device d
@@ -18,9 +18,4 @@ and s.is_own_device = 1 """)
     print 'Device:', device
     print 'Version:', version
 
-    with open('/home/edulution/id.txt') as idfile:
-       lines = map(lambda s: s.strip(), idfile.readlines())
-       for row in lines:
-           if len(row) > 0:
-              print row
 
